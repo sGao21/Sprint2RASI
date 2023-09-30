d855f4fdf9264b5109fd61f38b2205cf80e5adde
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-09-29 23:01:35 -0500

    Update historiasclinicas_logic.py

--- sprint2rasi/historiasclinicas/logic/historiasclinicas_logic.py
+++ sprint2rasi/historiasclinicas/logic/historiasclinicas_logic.py
@@ -1,5 +1,14 @@
 from ..models import HistoriaClinica
 
 def get_historias():
-    historias= HistoriaClinica.objects.all()
+    historias = HistoriaClinica.objects.all()
     return historias
+
+def get_historia(id):
+    historia = HistoriaClinica.objects.get(pk=id)
+    return historia
+
+def create_historia(form):
+    historia = form.save()
+    historia.save()
+    return ()
